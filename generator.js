4aac8d0d386541dacf6d0441b7b2a2f14b2ddaa7
Xavier Salazar <x.salazar1729@gmail.com>
2023-05-19 15:36:11 -0700

    Try injecting a tiny tiny image since Markdown will render it

--- generator.js
+++ generator.js
@@ -20,9 +20,9 @@ const fs = require("fs");
 // +-----------------------------------------------------------+ {14}
 
 function generateAquarium() {
-  let ret = "```\n"; // This will be written out to README.md
+  let ret = ""; // This will be written out to README.md
 
-  const emptySpace = "  ";
+  const emptySpace = "![](./empty.png)";
   const clouds = ["☁️", "🌧️", "⛈️", "🌩️"];
   const nightSkyTreats = ["🛸", "🚀", "⭐️", "☄️"];
   const wave = "🌊";
@@ -100,8 +100,6 @@ function generateAquarium() {
     // Start the next line
     ret += `\n`;
   }
-
-  ret += "```\n";
 
   fs.writeFileSync(`./README.md`, ret);
 }
